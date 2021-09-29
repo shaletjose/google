2756c77c962c33856b8c58756180f8249d36cb13
shaletjose <shaletjoseparathanath@gmail.com>
2021-09-29 14:52:21 +0530

    google(responsive)

--- static/js/usingjQuery1.js
+++ static/js/usingjQuery1.js
@@ -1,5 +1,5 @@
 var searched_content = localStorage.getItem("content1");
 setTimeout(function(){
     document.title=searched_content;
-    $('#scontent').val(searched_content);
-}, 0.1000);
+    $('#scontent').text(searched_content);
+ }, 0.1000);
